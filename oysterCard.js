32d92e0786f03a07635765e552e7717af32e6c9f
theara <theara2048@gmail.com>
2024-06-17 18:59:56 +0700

    feat: Add MetroService class with metro fare calculation

--- oysterCard.js
+++ oysterCard.js
@@ -15,5 +15,39 @@ class BusService {
   }
 }
 
+class MetroService {
+    constructor() {
+      this.fares = {
+        zone1: 2.5,
+        oneZoneOutsideZone1: 2.0,
+        twoZonesIncludingZone1: 3.0,
+        twoZonesExcludingZone1: 2.25,
+        moreThanTwoZones: 3.2,
+      };
+      this.maxFare = this.fares.moreThanTwoZones;
+    }
+  
+    calculateFare(startStation, endStation) {
+      const startZones = startStation.zones;
+      const endZones = endStation.zones;
+  
+      if (startZones.includes(1) && endZones.includes(1)) {
+        return this.fares.zone1;
+      } else if (startZones.length === 1 && endZones.length === 1 && startZones[0] !== 1 && endZones[0] !== 1) {
+        return this.fares.oneZoneOutsideZone1;
+      } else if (startZones.includes(1) || endZones.includes(1)) {
+        return this.fares.twoZonesIncludingZone1;
+      } else if (startZones.length > 1 || endZones.length > 1) {
+        return this.fares.twoZonesExcludingZone1;
+      } else {
+        return this.fares.moreThanTwoZones;
+      }
+    }
+  
+    getMaxFare() {
+      return this.maxFare;
+    }
+  }
 
-export { Station, BusService};
+
+export { Station, BusService, MetroService};
