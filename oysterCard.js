f17bd8f7c177bae5cd2ee90ba0ba4b4576aedf0a
theara <theara2048@gmail.com>
2024-06-19 22:42:33 +0700

    fix: fare calculation for noZone, oneZoneOutsideZone1,  twoZonesIncludingZone1 and twoZonesExcludingZone1

--- oysterCard.js
+++ oysterCard.js
@@ -1,3 +1,8 @@
+function isTwoZones(zone1, zone2) {
+  const diff = zone1 - zone2;
+  return diff === 1 || diff === -1;
+}
+
 class Station {
   constructor(name, zones) {
     this.name = name;
@@ -16,75 +21,92 @@ class BusService {
 }
 
 class MetroService {
-    constructor() {
-      this.fares = {
-        zone1: 2.5,
-        oneZoneOutsideZone1: 2.0,
-        twoZonesIncludingZone1: 3.0,
-        twoZonesExcludingZone1: 2.25,
-        moreThanTwoZones: 3.2,
-      };
-      this.maxFare = this.fares.moreThanTwoZones;
-    }
-  
-    calculateFare(startStation, endStation) {
-      const startZones = startStation.zones;
-      const endZones = endStation.zones;
-  
-      if (startZones.includes(1) && endZones.includes(1)) {
-        return this.fares.zone1;
-      } else if (startZones.length === 1 && endZones.length === 1 && startZones[0] !== 1 && endZones[0] !== 1) {
-        return this.fares.oneZoneOutsideZone1;
-      } else if (startZones.includes(1) || endZones.includes(1)) {
-        return this.fares.twoZonesIncludingZone1;
-      } else if (startZones.length > 1 || endZones.length > 1) {
-        return this.fares.twoZonesExcludingZone1;
-      } else {
-        return this.fares.moreThanTwoZones;
-      }
-    }
-  
-    getMaxFare() {
-      return this.maxFare;
+  constructor() {
+    this.fares = {
+      zone1: 2.5,
+      oneZoneOutsideZone1: 2.0,
+      twoZonesIncludingZone1: 3.0,
+      twoZonesExcludingZone1: 2.25,
+      moreThanTwoZones: 3.2,
+      noZone: 1.8,
+    };
+    this.maxFare = this.fares.moreThanTwoZones;
+  }
+
+  calculateFare(startStation, endStation) {
+    const startZones = startStation.zones;
+    const endZones = endStation.zones;
+
+    if (startZones.includes(1) && endZones.includes(1)) {
+      return this.fares.zone1;
+    } else if (startZones.length === 0 || endZones.length === 0) {
+      return this.fares.noZone;
+    } else if (
+      startZones.length === 1 &&
+      endZones.length === 1 &&
+      startZones[0] !== 1 &&
+      endZones[0] !== 1 &&
+      startZones[0] === endZones[0]
+    ) {
+      return this.fares.oneZoneOutsideZone1;
+    } else if (
+      startZones.includes(1) ||
+      (endZones.includes(1) &&
+        startZones[0] !== endZones[0] &&
+        isTwoZones(startZones[0], endZones[0]))
+    ) {
+      return this.fares.twoZonesIncludingZone1;
+    } else if (!startZones.includes(1) && !endZones.includes(1)) {
+      return this.fares.twoZonesExcludingZone1;
+    } else {
+      return this.fares.moreThanTwoZones;
     }
   }
 
-  class OysterCard {
-    constructor(busService, metroService, balance = 0) {
-      this.balance = balance;
-      this.busService = busService;
-      this.metroService = metroService;
-      this.currentJourney = null;
-    }
-  
-    load(amount) {
-      this.balance += amount;
-    }
-  
-    startJourney(station) {
-      if (this.currentJourney) throw new Error('Already in a journey');
-      if (this.balance < this.metroService.getMaxFare()) throw new Error('Insufficient balance');
-      this.currentJourney = { start: station, type: 'tube' };
-      this.balance -= this.metroService.getMaxFare();
-    }
-  
-    endJourney(station) {
-      if (!this.currentJourney) throw new Error('No journey in progress');
-      const fare = this.metroService.calculateFare(this.currentJourney.start, station);
-      this.balance += this.metroService.getMaxFare() - fare;
-      this.currentJourney = null;
-    }
-  
-    takeBus() {
-      if (this.balance < this.busService.getFare()) throw new Error('Insufficient balance');
-      this.balance -= this.busService.getFare();
-    }
-  
-    getBalance() {
-      return this.balance.toFixed(2);
-    }
+  getMaxFare() {
+    return this.maxFare;
   }
-  
+}
 
+class OysterCard {
+  constructor(busService, metroService, balance = 0) {
+    this.balance = balance;
+    this.busService = busService;
+    this.metroService = metroService;
+    this.currentJourney = null;
+  }
 
-export { Station, BusService, MetroService, OysterCard};
+  load(amount) {
+    this.balance += amount;
+  }
+
+  startJourney(station) {
+    if (this.currentJourney) throw new Error("Already in a journey");
+    if (this.balance < this.metroService.getMaxFare())
+      throw new Error("Insufficient balance");
+    this.currentJourney = { start: station, type: "tube" };
+    this.balance -= this.metroService.getMaxFare();
+  }
+
+  endJourney(station) {
+    if (!this.currentJourney) throw new Error("No journey in progress");
+    const fare = this.metroService.calculateFare(
+      this.currentJourney.start,
+      station
+    );
+    this.balance += this.metroService.getMaxFare() - fare;
+    this.currentJourney = null;
+  }
+
+  takeBus() {
+    if (this.balance < this.busService.getFare())
+      throw new Error("Insufficient balance");
+    this.balance -= this.busService.getFare();
+  }
+
+  getBalance() {
+    return this.balance.toFixed(2);
+  }
+}
+
+export { Station, BusService, MetroService, OysterCard };
